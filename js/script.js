db4aae2d369e291a8778b42f040f824a0c524301
Oranjeviy-Kisel <icosagon@rambler.ru>
2018-07-06 12:44:16 +0300

    Последние правки

--- js/script.js
+++ js/script.js
@@ -1,65 +1,65 @@
-			var link = document.querySelector("#write-us");
-			
-			var popup = document.querySelector(".modal-sendus");
-			var close = popup.querySelector(".modal-close");
-			
-			var form = popup.querySelector("form");
-			var usrname = popup.querySelector("[name=message-username]");
-			var usremail = popup.querySelector("[name=message-email]");
-			
-			var text = popup.querySelector("[name=message-text]");
-			
-			
-			link.addEventListener("click", function (evt) {
-				evt.preventDefault();
-				popup.classList.add("modal-show");
-				usrname.focus();
-			});
-			close.addEventListener("click", function (evt) {
-				evt.preventDefault();
+	var link = document.querySelector("#write-us");
+	
+	var popup = document.querySelector(".modal-sendus");
+	var close = popup.querySelector(".modal-close");
+	
+	var form = popup.querySelector("form");
+	var usrname = popup.querySelector("[name=message-username]");
+	var usremail = popup.querySelector("[name=message-email]");
+	
+	var text = popup.querySelector("[name=message-text]");
+	
+	
+	link.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		popup.classList.add("modal-show");
+		usrname.focus();
+	});
+	close.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		popup.classList.remove("modal-show");
+		popup.classList.remove("modal-error");
+	});
+	
+	form.addEventListener("submit", function (evt) {
+		if (!usrname.value || !usremail.value || !text.value) {
+			evt.preventDefault();
+			popup.classList.add("modal-error");
+			console.log("Заполните все поля!");
+		}
+	});
+	
+	window.addEventListener("keydown", function (evt) {
+		if (evt.keyCode === 27) {
+			evt.preventDefault();
+			if (popup.classList.contains("modal-show")) {
 				popup.classList.remove("modal-show");
 				popup.classList.remove("modal-error");
-			});
-			
-			form.addEventListener("submit", function (evt) {
-				if (!usrname.value || !usremail.value || !text.value) {
-					evt.preventDefault();
-					popup.classList.add("modal-error");
-					console.log("Заполните все поля!");
-				}
-			});
-			
-			window.addEventListener("keydown", function (evt) {
-				if (evt.keyCode === 27) {
-					evt.preventDefault();
-					if (popup.classList.contains("modal-show")) {
-						popup.classList.remove("modal-show");
-						popup.classList.remove("modal-error");
-					}
-				}
-			});
-			
-			
-			var mapLink = document.querySelector(".map-link");
+			}
+		}
+	});
+	
+	
+	var mapLink = document.querySelector(".map-link");
 
-			var mapPopup = document.querySelector(".modal-map");
-			var mapClose = mapPopup.querySelector(".modal-close");
-			
-			mapLink.addEventListener("click", function (evt) {
-				evt.preventDefault();
-				mapPopup.classList.add("modal-show");
-			});
+	var mapPopup = document.querySelector(".modal-map");
+	var mapClose = mapPopup.querySelector(".modal-close");
+	
+	mapLink.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		mapPopup.classList.add("modal-show");
+	});
 
-			mapClose.addEventListener("click", function (evt) {
-				evt.preventDefault();
+	mapClose.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		mapPopup.classList.remove("modal-show");
+	});
+
+	window.addEventListener("keydown", function (evt) {
+		if (evt.keyCode === 27) {
+			evt.preventDefault();
+			if (mapPopup.classList.contains("modal-show")) {
 				mapPopup.classList.remove("modal-show");
-			});
-
-			window.addEventListener("keydown", function (evt) {
-				if (evt.keyCode === 27) {
-					evt.preventDefault();
-					if (mapPopup.classList.contains("modal-show")) {
-					mapPopup.classList.remove("modal-show");
-					}
-				}
-			});
+			}
+		}
+	});
